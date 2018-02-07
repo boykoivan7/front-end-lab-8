22c3a2f57f835bac26c33ee48173f1fc01ee4c70
Ivan Boyko <boykoivan1998@gmail.com>
2018-02-07 14:07:22 +0200

    added homework_06

---
+++ homework_06/js/task1.js
@@ -0,0 +1,43 @@
+var a = parseFloat(prompt("Please enter length of side a", ""));
+var b = parseFloat(prompt("Please enter length of side b", ""));
+var c = parseFloat(prompt("Please enter length of side c", ""));
+
+if (a > 0 && b > 0 && c > 0){
+
+    /*Type of triangles:*/
+    var type;
+    /*1 - Equilateral*/
+    if(a==b&&a==c){
+        type="Equilateral";
+    }
+    /*2 - Isosceles*/
+    else if((a == b && a != c) || (a == c && a != b) || (b == c && b != a)){
+        type="Isosceles";
+    }
+    /*3 - Scalene*/
+    else if(a != b && a != c && b != c){
+        /*4 - Right triangle*/
+        /*Pythagorean theorem*/
+        if ((a * a + b * b == c * c) || (a * a + c * c == b * b) || (c * c + b * b == a * a)) {
+            type="Right triangle";
+        } 
+        /*3 - only Scalene*/
+        else {
+            type="Scalene";
+        }
+    }
+
+    /* Square - Heron's formula*/
+    var p = (a + b + c) / 2; //semiperimeter
+    var square = Math.sqrt(p * (p - a) * (p - b) * (p - c));
+    square = Math.round(square * 100) / 100; //round
+
+    var output;
+    output = "Type of triangle is " + type + " and square is " + square;
+
+    console.log(output);
+    }
+
+else {
+    console.log("Incorrect data");
+}
---
+++ homework_06/js/task2.js
@@ -0,0 +1,28 @@
+/*Currency EUR/UAH: 1 EUR = 33.8565 UAH; - 07.02.18 13:34*/
+/*Currency USD/UAH: 1 USD = 27.4609 UAH; - 07.02.18 13:34*/
+
+var euro = 33.8565;
+var dollar = 27.4609;
+
+var euroAmount = parseFloat(prompt("Please enter amount of EURO", ""));
+var dollarAmount = parseFloat(prompt("Please enter amount of USD", ""));
+
+if (euroAmount > 0 && dollarAmount > 0){
+    var hryvniaAmountEUR = euroAmount * euro;
+    hryvniaAmountEUR = Math.round(hryvniaAmountEUR * 1000) / 1000; //round
+
+    var hryvniaAmountUSD = dollarAmount * dollar;
+    hryvniaAmountUSD = Math.round(hryvniaAmountUSD * 1000) / 1000; //round
+
+    var EUR2USDCurrency = euro/dollar;
+    EUR2USDCurrency = Math.round(EUR2USDCurrency * 1000) / 1000; //round
+
+    var output;
+    output = euroAmount + " euros are equal  " + hryvniaAmountEUR + "UAH, " + dollarAmount + " are equal " + hryvniaAmountUSD + "UAH, one euro is equal " + EUR2USDCurrency + " dollars";
+
+    console.log(output);
+}
+
+else {
+    console.log("Incorrect data");
+}
