318edb70d0aaa501e18d56b771730b7ddf13d478
Ivan Boyko <boykoivan1998@gmail.com>
2018-02-07 21:06:42 +0200

    changed round to 2 numbers after .

--- homework_06/js/task2.js
+++ homework_06/js/task2.js
@@ -7,22 +7,20 @@ var dollar = 27.4609;
 var euroAmount = parseFloat(prompt("Please enter amount of EURO", ""));
 var dollarAmount = parseFloat(prompt("Please enter amount of USD", ""));
 
-if (euroAmount > 0 && dollarAmount > 0){
+if (euroAmount > 0 && dollarAmount > 0) {
     var hryvniaAmountEUR = euroAmount * euro;
-    hryvniaAmountEUR = Math.round(hryvniaAmountEUR * 1000) / 1000; //round
+    hryvniaAmountEUR = Math.round(hryvniaAmountEUR * 100) / 100; //round
 
     var hryvniaAmountUSD = dollarAmount * dollar;
-    hryvniaAmountUSD = Math.round(hryvniaAmountUSD * 1000) / 1000; //round
+    hryvniaAmountUSD = Math.round(hryvniaAmountUSD * 100) / 100; //round
 
-    var EUR2USDCurrency = euro/dollar;
-    EUR2USDCurrency = Math.round(EUR2USDCurrency * 1000) / 1000; //round
+    var EUR2USDCurrency = euro / dollar;
+    EUR2USDCurrency = Math.round(EUR2USDCurrency * 100) / 100; //round
 
     var output;
     output = euroAmount + " euros are equal  " + hryvniaAmountEUR + "UAH, " + dollarAmount + " are equal " + hryvniaAmountUSD + "UAH, one euro is equal " + EUR2USDCurrency + " dollars";
 
     console.log(output);
-}
-
-else {
+} else {
     console.log("Incorrect data");
 }
