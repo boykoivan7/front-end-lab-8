632719be63c6d58a9115d1b2fb6098fdc4db272d
Ivan Boyko <boykoivan1998@gmail.com>
2018-02-10 00:59:10 +0200

    make readable

--- homework_07/js/task1.js
+++ homework_07/js/task1.js
@@ -1,20 +1,20 @@
 var numberString = prompt("Enter natural number N (0<N<=20)", "");
-var checkNumber = !isNaN(parseFloat(numberString)) && isFinite(numberString) && Number.isInteger(parseFloat(numberString)) && parseFloat(numberString) > 0 && parseFloat(numberString) <= 20 ;
+var checkNumber = !isNaN(parseFloat(numberString)) && isFinite(numberString) && Number.isInteger(parseFloat(numberString)) && parseFloat(numberString) > 0 && parseFloat(numberString) <= 20;
 if (checkNumber) {
-	var number = parseInt(numberString);
+    var number = parseInt(numberString);
     var symbol = "[~]";
     var empty = "   ";
     var string = "";
-    for(var row = 1; row <= number; row++){
+    for (var row = 1; row <= number; row++) {
         var emptyNumber = number - row; //number of empty symbols (3 space) before symbols
         var symbolNumber = row * 2 - 1; //number of symbols in row
-        for(var j = 0; j < emptyNumber; j++){
+        for (var j = 0; j < emptyNumber; j++) {
             string += empty;
             string += " ";
         }
-        for(var j = 0; j < symbolNumber; j++){
+        for (var j = 0; j < symbolNumber; j++) {
             string += symbol;
-            if(j != symbolNumber - 1){
+            if (j != symbolNumber - 1) {
                 string += " ";
             } else {
                 string += "\n"
