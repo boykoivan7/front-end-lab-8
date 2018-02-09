d237c6e4708763077059f91466f82cebda2e23fe
Ivan Boyko <boykoivan1998@gmail.com>
2018-02-10 00:43:10 +0200

    add new method

--- homework_07/js/task2.js
+++ homework_07/js/task2.js
@@ -8,6 +8,76 @@ if(isPlay) {
     var attemptsLeft;
     var possiblePrizeNow;
     var number; //user number
+	var startGame = true;
+	
+	while(startGame){
+		randomMin = 0;
+		randomMax = 5;
+		maxPrize = 10;
+		totalPrize = 0;		
+		var startRandom = true;	
+		
+		while(startRandom){
+			attemptsLeft = 3;
+			possiblePrizeNow = maxPrize;
+			randomNumber = Math.floor(Math.random() * (randomMax - randomMin + 1) + randomMin);			
+			var enterNumber = true;	
+			
+			while(enterNumber) {
+				var textPrompt = "Enter number from " + randomMin + " to " + randomMax + "\nAttempts left: " + attemptsLeft + "\nTotal prize: " + totalPrize + "$\nPossible prize on current attempt: " + possiblePrizeNow + "$";
+				number = parseInt(prompt(textPrompt, ""));				
+				if (number == randomNumber) {
+					totalPrize += possiblePrizeNow;
+					var isContinue = confirm("You win!!! Do you want to continue a game?");
+					if(isContinue) {
+						randomMax *= 2;
+						maxPrize *= 3;
+						break; //go to StartRandom
+					} else {
+						console.log("Thank you for a game. Your prize is: " + totalPrize + "$");
+						var isTryAgain = confirm("Do you want to try again?");
+						if(!isTryAgain){
+							startGame = false;
+						}
+						startRandom = false;
+						break;
+					}
+				}
+				else {
+					if(attemptsLeft == 1){
+						console.log("Thank you for a game. Your prize is: " + totalPrize + "$");
+						var isTryAgain = confirm("Do you want to try again?");
+						if(!isTryAgain){
+							startGame = false;
+						}
+						startRandom = false; 
+						break;
+						
+					} else {
+						attemptsLeft--;
+						possiblePrizeNow = Math.floor(possiblePrizeNow/2);
+						continue; //next attempt
+					} 
+				}
+			}	
+		}		
+	}
+		
+} else {
+    console.log("You did not become a millionaire");
+}
+
+/* 2 method with functions
+var isPlay = confirm("Do you want to play a game?");
+
+if(isPlay) {
+    var randomMin;
+    var randomMax;
+    var maxPrize;
+    var randomNumber;
+    var attemptsLeft;
+    var possiblePrizeNow;
+    var number; //user number
     startGame();
 } else {
     console.log("You did not become a millionaire");
@@ -69,3 +139,4 @@ function checkNumber() {
 function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min);
 }
+*/
